f807de0e9488ff3185a985b95e99862549f0f3a7
Vishal <vishal@geekyants.com>
2018-07-04 17:24:37 +0530

    added env.port

--- index.js
+++ index.js
@@ -172,6 +172,6 @@ app.get("/users", function(req, res) {
   res.status(200).send(users);
 });
 
-server.listen(5000, () => {
-  console.log("Listening on port 5000");
+server.listen(process.env.PORT || 5000, () => {
+  // console.log("Listening on port " + process.env.PORT || 5000);
 });
